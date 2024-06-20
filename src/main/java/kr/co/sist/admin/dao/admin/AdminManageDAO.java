e3b6735a5dbfa08c729abea8401db87f911d444c
jmh <tellis0207@gmail.com>
2024-06-20 18:00:36 +0900

    feat : springframework.security dependency 추가, 암호화를 위한 패키지 구성 추가

--- src/main/java/kr/co/sist/admin/dao/admin/AdminManageDAO.java
+++ src/main/java/kr/co/sist/admin/dao/admin/AdminManageDAO.java
@@ -1,7 +1,12 @@
 package kr.co.sist.admin.dao.admin;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.ibatis.exceptions.PersistenceException;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Component;
+import kr.co.sist.admin.domain.admin.AdminInfoDomain;
+import kr.co.sist.admin.domain.admin.LoginDomain;
 import kr.co.sist.admin.vo.admin.LoginVO;
 import kr.co.sist.properties.MyBatisConfig;
 
@@ -15,15 +20,27 @@ public class AdminManageDAO {
 
 
 
-    public String selectLogin(LoginVO lVO) {
+    public LoginDomain selectLogin(LoginVO lVO) throws PersistenceException {
         String adminId = "";
         SqlSession ss = myBatis.getMyBatisHandler(false);
 
-        adminId = ss.selectOne("kr.co.sist.admin.admin.selectLogin", lVO);
-        System.out.println(adminId);
+        LoginDomain ld = ss.selectOne("kr.co.sist.admin.admin.selectLogin", lVO);
+        System.out.println(ld.getAdminId());
         myBatis.closeHandler(ss);
 
-        return adminId;
+        return ld;
     }// selectLogin
+
+    public List<AdminInfoDomain> selectAdminList() {
+        List<AdminInfoDomain> list = new ArrayList<AdminInfoDomain>();
+
+        SqlSession ss = myBatis.getMyBatisHandler(false);
+
+        list = ss.selectList("kr.co.sist.admin.admin.selectAdminList");
+
+        myBatis.closeHandler(ss);
+
+        return list;
+    }// selectAdminList
 
 }
