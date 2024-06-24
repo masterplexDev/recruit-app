b2d42baac9d5fe29d57f36e1b0ce7bc97445a0c9
devpanic <yeon.dev.8@gmail.com>
2024-06-24 16:49:11 +0900

    feat : 관리자 채용 공고 리스트 조회 맵퍼 추가

--- src/main/java/kr/co/sist/admin/dao/resume/ResumeAdminDAO.java
+++ src/main/java/kr/co/sist/admin/dao/resume/ResumeAdminDAO.java
@@ -1,9 +1,13 @@
 package kr.co.sist.admin.dao.resume;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import kr.co.sist.admin.domain.resume.ResumeDomain;
+import kr.co.sist.admin.domain.resume.ResumeListDomain;
+import kr.co.sist.admin.vo.resume.SearchVO;
 import kr.co.sist.properties.MyBatisConfig;
 
 @Component
@@ -15,8 +19,11 @@ public class ResumeAdminDAO {
         this.myBatis = myBatis;
     }
 
-    public List<ResumeDomain> selectResumes() {
-        List<ResumeDomain> resumes = null;
+    public List<ResumeListDomain> selectResumes(SearchVO searchVO) {
+        SqlSession session = myBatis.getMyBatisHandler(false);
+        List<ResumeListDomain> resumes = new ArrayList<ResumeListDomain>();
+        resumes = session.selectList("kr.co.sist.resume.admin.selectResumes", searchVO);
+        myBatis.closeHandler(session);
 
         return resumes;
     }
