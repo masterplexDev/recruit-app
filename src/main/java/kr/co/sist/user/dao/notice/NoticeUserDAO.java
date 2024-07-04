deef7defc6520dd7db4fc57ae6258bb44894cc32
psh <ppluvsh@gmail.com>
2024-07-04 15:49:47 +0900

    feat : notice user 카테고리별 검색 기능 구현 qna user, admin 글번호 수정

--- src/main/java/kr/co/sist/user/dao/notice/NoticeUserDAO.java
+++ src/main/java/kr/co/sist/user/dao/notice/NoticeUserDAO.java
@@ -24,6 +24,49 @@ public class NoticeUserDAO {
         return noticeList;
     }
 
+    // public List<NoticeUserDomain> selectNoticesByCategory(String category) {
+    // SqlSession session = myBatis.getMyBatisHandler(false);
+    // List<NoticeUserDomain> noticeList =
+    // session.selectList("kr.co.sist.notice.user.selectNoticesByCategory", category);
+    // myBatis.closeHandler(session);
+    // return noticeList;
+    // }
+
+    public List<NoticeUserDomain> selectNoticesByCategory(String category) {
+        SqlSession session = myBatis.getMyBatisHandler(false);
+        try {
+            System.out.println("=============" + category);
+            // MyBatis를 통해 SQL 실행
+            List<NoticeUserDomain> noticeList =
+                    session.selectList("kr.co.sist.notice.user.selectNoticesByCategory", category);
+
+            System.out.println("===== 결과 리스트 ========");
+            for (NoticeUserDomain notice : noticeList) {
+                System.out.println(notice);
+            }
+            return noticeList; // 결과 리스트
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("공지사항 조회 중 오류 발생", e);
+        }
+    }
+
+    // public List<NoticeUserDomain> selectNotices(String category) {
+    // SqlSession session = myBatis.getMyBatisHandler(false);
+    // List<NoticeUserDomain> noticeList;
+    // try {
+    // if (category != null && !category.isEmpty() && !"전체".equals(category)) {
+    // noticeList = session.selectList(
+    // "kr.co.sist.user.domain.notice.selectNoticesByCategory", category);
+    // } else {
+    // noticeList = session.selectList("kr.co.sist.user.domain.notice.selectNotices");
+    // }
+    // } finally {
+    // myBatis.closeHandler(session);
+    // }
+    // return noticeList;
+    // }
+
     public NoticeUserDomain selectOneNotice(int notice_num) {
         SqlSession session = myBatis.getMyBatisHandler(false);
         NoticeUserDomain noticeDtail =
