360d6951f0fc86b69716da21bf27ed3240484b4b
psh <ppluvsh@gmail.com>
2024-07-04 18:39:21 +0900

    feat : 공지사항 카테고리 항목 전체 목록 불러오기 수정 공지사항 검색기능 구현 중

---
+++ src/main/java/kr/co/sist/user/vo/notice/SearchVO.java
@@ -0,0 +1,16 @@
+package kr.co.sist.user.vo.notice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SearchVO {
+    private String keyword;
+}
