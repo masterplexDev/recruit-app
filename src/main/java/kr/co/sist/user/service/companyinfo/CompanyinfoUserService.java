70e98719512530dc3120868ccb81b7c9dd5dbe5f
devpanic <yeon.dev.8@gmail.com>
2024-07-05 14:42:10 +0900

    chore : 콘솔 한글 삭제

--- src/main/java/kr/co/sist/user/service/companyinfo/CompanyinfoUserService.java
+++ src/main/java/kr/co/sist/user/service/companyinfo/CompanyinfoUserService.java
@@ -11,60 +11,62 @@ import kr.co.sist.vo.companyinfo.SearchVO;
 
 @Service
 public class CompanyinfoUserService {
-    
-    @Autowired(required=false)
+
+    @Autowired(required = false)
     private CompanyinfoUserDAO companyinfoUserDAO;
 
     public List<SearchDomain> searchAllCompanyinfo() {
-        System.out.println("ÀÌ°Ç ¼­ºñ½º¾ß");
-        
-        List<SearchDomain> list=null;
-        list=companyinfoUserDAO.selectAllCompanyinfo();
-        
+        // System.out.println("ï¿½Ì°ï¿½ ï¿½ï¿½ï¿½ñ½º¾ï¿½");
+
+        List<SearchDomain> list = null;
+        list = companyinfoUserDAO.selectAllCompanyinfo();
+
         return list;
-//        return companyinfoUserDAO.selectAllCompanyinfo();
+        // return companyinfoUserDAO.selectAllCompanyinfo();
     }
 
     public List<SearchDomain> searchCompanyinfoList(SearchVO sVO) {
-        
+
         return companyinfoUserDAO.selectCompanyinfoList(sVO);
     }
-    
+
     public List<SearchDomain> searchCompanyinfo(Map<String, Object> params) {
-        System.out.println("ÀÌ°Ç serviceÀÇ params¾ß : "+params);
-//        if("companyClassification"==null) {
-//            
-//        }
+        // System.out.println("ï¿½Ì°ï¿½ serviceï¿½ï¿½ paramsï¿½ï¿½ : "+params);
+        // if("companyClassification"==null) {
+        //
+        // }
         return companyinfoUserDAO.selectCompanyinfo(params);
     }
-    
-    
-    public List<SearchDomain> searchCompanyinfoDetail(String companyCode){
-        List<SearchDomain> list=null;
+
+
+    public List<SearchDomain> searchCompanyinfoDetail(String companyCode) {
+        List<SearchDomain> list = null;
         try {
-            list=companyinfoUserDAO.selectCompanyinfoDetail(companyCode);
-            
-        }catch(PersistenceException pe) {
+            list = companyinfoUserDAO.selectCompanyinfoDetail(companyCode);
+
+        } catch (PersistenceException pe) {
             pe.printStackTrace();
         }
         return list;
     }
-    public List<SearchDomain> searchHistory(String companyCode){
-        List<SearchDomain> list=null;
+
+    public List<SearchDomain> searchHistory(String companyCode) {
+        List<SearchDomain> list = null;
         try {
-            list=companyinfoUserDAO.selectHistory(companyCode);
-            
-        }catch(PersistenceException pe) {
+            list = companyinfoUserDAO.selectHistory(companyCode);
+
+        } catch (PersistenceException pe) {
             pe.printStackTrace();
         }
         return list;
     }
-    public List<SearchDomain> searchWelfare(String companyCode){
-        List<SearchDomain> list=null;
+
+    public List<SearchDomain> searchWelfare(String companyCode) {
+        List<SearchDomain> list = null;
         try {
-            list=companyinfoUserDAO.selectWelfare(companyCode);
-            
-        }catch(PersistenceException pe) {
+            list = companyinfoUserDAO.selectWelfare(companyCode);
+
+        } catch (PersistenceException pe) {
             pe.printStackTrace();
         }
         return list;
